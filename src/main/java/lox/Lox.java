80652f364cabef3cca566ab6dbbc06f5c1f3ff4a
SR71 <uday.kir.k@gmail.com>
2024-09-10 12:56:34 +0530

    Update Lox.java

--- src/main/java/lox/Lox.java
+++ src/main/java/lox/Lox.java
@@ -2,7 +2,6 @@ package lox;
 
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
@@ -13,6 +12,7 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class Lox {
+    static boolean hadError = false;
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -31,19 +31,41 @@ public class Lox {
             String line = bufferedReader.readLine();
             if (line == null) break;
             run(line);
+            hadError = false;
         }
     }
 
     private static void runFile(String filePath) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(filePath));
         run(new String(bytes, Charset.defaultCharset()));
+        if(hadError){
+            //Here we are following the BSD error codes : https://www.freebsd.org/cgi/man.cgi?query=sysexits&apropos=0&sektion=0&manpath=FreeBSD+4.3-RELEASE&format=html
+            /*
+               EX_DATAERR (65)	   The input data was incorrect in some way.  This
+			   should only be used for user's data and not system
+			   files.
+             */
+            System.exit(65);
+        }
+
     }
 
     private static void run(String contents) {
-        //Lets print the tokens in this chapter
+        //Let's print the tokens in this chapter
 
         Scanner scanner = new Scanner(contents);
         Stream<String> tokens = scanner.tokens();
         tokens.forEach(System.out::println);
+    }
+
+    //not used for now, to be used in further steps
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+    private static void report(int line, String where,
+                               String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 }
