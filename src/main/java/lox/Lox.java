458acc2a228da6d02b983cefb4ba101fad65f1c9
SR71 <uday.kir.k@gmail.com>
2024-09-10 11:32:14 +0530

    Update Lox runner

--- src/main/java/lox/Lox.java
+++ src/main/java/lox/Lox.java
@@ -1,7 +1,42 @@
 package lox;
 
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Lox {
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+        } else if (args.length == 0) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        while (true) {
+            System.out.print(">");
+            String line = bufferedReader.readLine();
+            if (line == null) break;
+            run(line);
+        }
+    }
+
+    private static void runFile(String filePath) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void run(String string) {
+        throw new UnsupportedOperationException("Test");
     }
 }
